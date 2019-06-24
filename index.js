5d8e2600d787a6aa26536c667311ee486cd79580
Notarangelo-smeup <51441272+Notarangelo-smeup@users.noreply.github.com>
2019-06-24 10:06:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,14 @@ restService.post("/echo", function(req, res) {
         items: [
           {
             simpleResponse: {
-              textToSpeech: req.body.queryResult &&
+               var speech =
+    req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
+              textToSpeech: speech
+  
             }
           }
         ]
