f9556b9896e28abcd2b99a04886cb3df213da0ca
Notarangelo-smeup <51441272+Notarangelo-smeup@users.noreply.github.com>
2019-06-26 15:04:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ restService.post("/echo", function(req, res) {
           {
             simpleResponse: {
              
-              textToSpeech:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"periodofinale+" " +"intento:"+intento
+              textToSpeech:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"+periodofinale+" " +"intento:"+intento
               
               
             
@@ -59,9 +59,9 @@ restService.post("/echo", function(req, res) {
   return res.json({
     payload: temp,
     data: temp,
-    fulfillmentText:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"periodofinale+" " +"intento:"+intento,
-    speech:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"periodofinale+" " +"intento:"+intento,
-    displayText:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"periodofinale+" " +"intento:"+intento,
+    fulfillmentText:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"+periodofinale+" " +"intento:"+intento,
+    speech:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"+periodofinale+" " +"intento:"+intento,
+    displayText:"ti mostro le attività di " + persona +" " + "nel periodo da:" + periodoiniziale+"-"+periodofinale+" " +"intento:"+intento,
     source: "webhook-echo-sample1"
   });
 });
