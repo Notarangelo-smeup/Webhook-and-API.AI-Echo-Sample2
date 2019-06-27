58c5e7089e16f11ef4c1486d426ee8d4c9f3ae3b
Notarangelo-smeup <51441272+Notarangelo-smeup@users.noreply.github.com>
2019-06-27 10:17:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
    //var speech =  "ciao";
-   var persona = req.body.queryResult.outputContexts[1].parameters.persona.name;
+   //var persona = req.body.queryResult.outputContexts[1].parameters.persona.name;
   
      var  periodoiniziale=req.body.queryResult.outputContexts[0].parameters.periodo.startDate;
      var periodofinale=req.body.queryResult.outputContexts[0].parameters.periodo.endDate;
@@ -39,7 +39,7 @@ restService.post("/echo", function(req, res) {
     }
   };
 //  var speech ="ciao";
- var persona = req.body.queryResult.outputContexts[1].parameters.persona.name;
+// var persona = req.body.queryResult.outputContexts[1].parameters.persona.name;
   var  periodoiniziale=req.body.queryResult.outputContexts[0].parameters.periodo.startDate;
   var periodofinale=req.body.queryResult.outputContexts[0].parameters.periodo.endDate;
   var intento= req.body.queryResult.intent.displayName;
